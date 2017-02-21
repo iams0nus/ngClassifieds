4fa4f3eb5de47806292fe597b31e04c65820ef83
iams0nus <sharmasonu.edu@gmail.com>
2017-02-22 00:57:15 +0530

    Add Deletion, Add Generic Search, Add Category Search

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -1,62 +1,86 @@
 (function(){
 	'use strict';
 	angular.module('ngClassifieds')
-	.controller('classifiedsCtrl',['$scope', '$http', 'classifiedsFactory', '$mdSidenav', '$mdToast', function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
-		
-		classifiedsFactory.getClassifieds().then(function(classifieds){
-			$scope.classifieds = classifieds.data;
-		});
+	.controller('classifiedsCtrl',['$scope', '$http', 'classifiedsFactory', '$mdSidenav', '$mdToast', '$mdDialog', 
+		function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog){
 
-		$scope.openSidebar = function(){
-			$mdSidenav('left').open();
-		};
-		$scope.closeSidebar = function(){
-			$mdSidenav('left').close();
-		};
+			classifiedsFactory.getClassifieds().then(function(classifieds){
+				$scope.classifieds = classifieds.data;
+				$scope.categories=getCategories($scope.classifieds);
+			});
 
-		var contact = {
-			name:'Sonu Sharma',
-			phone:"(842) 035-0246",
-			email:"sharmasonu.edu@gmail.com"
-		};
+			$scope.openSidebar = function(){
+				$mdSidenav('left').open();
+			};
+			$scope.closeSidebar = function(){
+				$mdSidenav('left').close();
+			};
 
-		$scope.saveClassified = function(classified){
-			if(classified){
-				classified.contact = contact;
-				$scope.classifieds.push(classified);
+			var contact = {
+				name:'Sonu Sharma',
+				phone:"(842) 035-0246",
+				email:"sharmasonu.edu@gmail.com"
+			};
+
+			$scope.saveClassified = function(classified){
+				if(classified){
+					classified.contact = contact;
+					$scope.classifieds.push(classified);
+					$scope.classified = {};
+					$scope.closeSidebar();
+					showToast("Classified Saved!!");
+				}	
+			};
+
+			$scope.editClassified = function(classified){
+				$scope.editing = true;
+				$scope.openSidebar();
+				$scope.classified = classified;
+			};
+
+			$scope.saveEdit = function(){
+				$scope.editing = false;
 				$scope.classified = {};
 				$scope.closeSidebar();
-				showToast("Classified Saved!!");
-			}	
-		};
+				showToast("Classified Edited!!");
+			};
 
-		$scope.editClassified = function(classified){
-			$scope.editing = true;
-			$scope.openSidebar();
-			$scope.classified = classified;
-		};
-
-		$scope.saveEdit = function(){
-			$scope.editing = false;
-			$scope.classified = {};
-			$scope.closeSidebar();
-			showToast("Classified Edited!!");
-		};
-
-		function showToast(message){
-			$mdToast.show(
+			function showToast(message){
+				$mdToast.show(
 					$mdToast.simple()
-						.content(message)
-						.position('top, right')
-						.hideDelay(3000)
+					.content(message)
+					.position('top, right')
+					.hideDelay(3000)
 				);
-		};
+			};
 
-		$scope.deleteClassified = function(classified){
-			var index = $scope.classified.indexOf(classified);
-			$scope.classifieds.splice(classified);
-		}
+			$scope.deleteClassified = function(event, classified){
+				$mdDialog.show(
+					$mdDialog.confirm()
+					.title('Are you sure you want to delete ' + classified.title + '?')
+					.ok('Delete')
+					.cancel('Go Back')
+					.targetEvent(event)
+				)
+				.then(function(){
+					var index = $scope.classifieds.indexOf(classified);
+					$scope.classifieds.splice(index ,1);
+					showToast("Classified Deleted!!")
+				},function(){});
+				
+			};
 
+			
 
-	}]);
+			function getCategories(classifieds){
+				var categories =[];
+				angular.forEach(classifieds, function(item){
+					angular.forEach(item.categories, function(category){
+						categories.push(category);
+					});
+				});
+				return _.uniq(categories);
+			};
+
+		}]);
 })();
--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 angular.module("ngClassifieds",['ngMaterial'])
 	.config(function($mdThemingProvider){
 		$mdThemingProvider.theme('default')
-			.primaryPalette('teal')
-			.accentPalette('orange');
+			.primaryPalette('cyan')
+			.accentPalette('lime');
 
 	});
+
+	// Available palettes: red, pink, purple, deep-purple, indigo, blue, light-blue, cyan, teal, green, light-green, lime, yellow, amber, orange, deep-orange, brown, grey, blue-grey
